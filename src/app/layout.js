6e57a3daed0eb8f132cd7e4d038b61829e64aeaf
Lutfor Rahman <lutforr3845@gmail.com>
2025-01-23 14:56:49 +0600

    navbar style

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { Navbar } from "@/components/Navbar";
+import { Navbar } from "@/Components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
