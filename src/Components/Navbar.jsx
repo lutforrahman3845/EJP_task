8832ca31a0f939017d6c99529dece710ef8c69cd
Lutfor Rahman <lutforr3845@gmail.com>
2025-01-23 14:13:49 +0600

    update style

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,32 +13,30 @@ export const Navbar = async () => {
     <div className="bg-gray-800 text-white p-4">
       <nav className="flex justify-between items-center">
         <div className="text-lg font-semibold ">Logo</div>
-        <div>
-          <ul className="flex space-x-4 ">
-            <li className="py-1 px-2 border rounded font-medium">
-              <Link href={"/"}>Home</Link>
-            </li>
-            <li className="py-1 px-2 border rounded font-medium">
-              <Link href={"/profile"}>Profile</Link>
-            </li>
-            {user ? (
-              <>
-                <li className="py-1 px-2 border rounded font-medium">
-                  <LogoutLink>Log out</LogoutLink>
-                </li>
-              </>
-            ) : (
-              <>
-                <li className="py-1 px-2 border rounded font-medium">
-                  <LoginLink>Log in</LoginLink>
-                </li>
-                <li className="py-1 px-2 border rounded font-medium">
-                  <RegisterLink>Register</RegisterLink>
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
+        <ul className="flex space-x-4 ">
+          <li className="py-1 px-2 border rounded font-medium">
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li className="py-1 px-2 border rounded font-medium">
+            <Link href={"/profile"}>Profile</Link>
+          </li>
+          {user ? (
+            <>
+              <li className="py-1 px-2 border rounded font-medium">
+                <LogoutLink>Log out</LogoutLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="py-1 px-2 border rounded font-medium">
+                <LoginLink>Log in</LoginLink>
+              </li>
+              <li className="py-1 px-2 border rounded font-medium">
+                <RegisterLink>Register</RegisterLink>
+              </li>
+            </>
+          )}
+        </ul>
       </nav>
     </div>
   );
